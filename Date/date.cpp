c10da5c81746ff456980ece7b7258f342187ad97
Seth Roberts <sar242004@gmail.com>
2025-02-28 18:47:19 -0600

    2/28/2025 6:43PM

--- Date/date.cpp
+++ Date/date.cpp
@@ -13,7 +13,7 @@ Date::Date() {
 
 }
 
-Date::Date(int d, int m, int y) { // Make this check if the date enterd is valid and if it is not throw Date::Invalid
+Date::Date(int d, int m, int y) {
 	time(&_t);
 	_date = *localtime(&_t);
 
@@ -41,15 +41,45 @@ const int Date::year() {
 	return (_date.tm_year + 1900);
 };
 
-void Date::day(int day) {	//If setting the field to the required value would result in an invalid date,
-	_date.tm_mday = day;	// throw an Date::Invalid exception containing the values your date would have
-};							// had. Setters should have a strong exception guarantee. (FOR ALL 3 SETTERS)
+void Date::day(int day) {
+	tm dateCpy = _date;
+	int originalDay = day;
+	dateCpy.tm_mday = day;
+
+	time_t t = mktime(&dateCpy);
+
+	if (dateCpy.tm_mday != originalDay || dateCpy.tm_mon != _date.tm_mon || dateCpy.tm_year != _date.tm_year) {
+		throw Date::Invalid{ originalDay, _date.tm_mon + 1, _date.tm_year + 1900 };
+	}
+
+	_date.tm_mday = originalDay;
+}
 
 void Date::month(int month) {
-	_date.tm_mon = (month - 1);
-};
+	tm dateCpy = _date;
+	int originalMonth = month;
+	dateCpy.tm_mon = month - 1;
 
-void Date::year(int year) {
-	_date.tm_year = (year - 1900);
+	time_t t = mktime(&dateCpy);
+
+	if (dateCpy.tm_mday != _date.tm_mday || dateCpy.tm_mon != originalMonth - 1 || dateCpy.tm_year != _date.tm_year) {
+		throw Date::Invalid{ _date.tm_mday, originalMonth, _date.tm_year + 1900 };
+	}
+
+	_date.tm_mon = originalMonth - 1;
+}
+
+void Date::year(int year) { //Is there such a thing as an invalid year? 
+	tm dateCpy = _date;
+	int originalYear = year;
+    dateCpy.tm_year = year - 1900;
+
+    time_t t = mktime(&dateCpy);
+
+    if (dateCpy.tm_mday != _date.tm_mday || dateCpy.tm_mon != _date.tm_mon || dateCpy.tm_year != originalYear - 1900) {
+        throw Date::Invalid{ _date.tm_mday, _date.tm_mon + 1, originalYear };
+    }
+
+    _date.tm_year = originalYear - 1900;
 };
 
--- DateTest/date.test.cpp
+++ DateTest/date.test.cpp
@@ -24,3 +24,29 @@ TEST(errorTesting, InvalidDay)
         EXPECT_EQ(error.year, 2020);
     }
 }
+TEST(errorTesting, InvalidDaySetter)
+{
+    try {
+        Date date(28, 2, 2025);
+        date.day(29);
+        FAIL() << "Expected Date::Invalid{ 29, 2, 2025 }";
+    }
+    catch (const Date::Invalid& error) {
+        EXPECT_EQ(error.day, 29);
+        EXPECT_EQ(error.month, 2);
+        EXPECT_EQ(error.year, 2025);
+    }
+}
+TEST(errorTesting, InvalidMonthSetter)
+{
+    try {
+        Date date(20, 12, 2025);
+        date.month(13);
+        FAIL() << "Expected Date::Invalid{ 20, 13, 2025 }";
+    }
+    catch (const Date::Invalid& error) {
+        EXPECT_EQ(error.day, 20);
+        EXPECT_EQ(error.month, 13);
+        EXPECT_EQ(error.year, 2025);
+    }
+}
